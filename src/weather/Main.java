d6dc5272ae08c77ec354731360cba22143d4d106
megan nickel <megansyd@live.unc.edu>
2020-10-17 15:34:22 -0400

    testing

--- src/weather/Main.java
+++ src/weather/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         WeatherUtil weather = new WeatherUtil("historysummary_data.csv");
         double temp = weather.getYearData(2002).getAvgTemp()[0];
-        //System.out.println(temp);
+        //System.out.println(temp2);
     }
 }
 
